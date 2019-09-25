4026d8e46ae8ac043b0f58bac7faa4632617b8ec
g4brieljs <gabrieljimenez690@gmail.com>
2019-09-25 16:16:45 -0400

    Axios in nextjs
    Add axios in Nextjs

--- components/MasterPage.js
+++ components/MasterPage.js
@@ -6,12 +6,13 @@ const MasterPage = (props) => (
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             <link rel="stylesheet" href="https://bootswatch.com/4/litera/bootstrap.min.css" />
-            <title>Bitcoin</title>
+            <title>AllBitcoin</title>
         </Head>
 
         <Navegacion />
-
-        {props.children}
+        <div className="container mt-4">
+            {props.children}
+        </div>
     </div>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,30 @@
 import MasterPage from '../components/MasterPage';
+import axios from 'axios';
 
 
-const Index = () => (
+const Index = (props) => (
     <MasterPage>
-        <div>
-            <p>DESDE INICIO</p>
+        {/* Todo lo que este acá dentro es el {props.children} de MasterPage */}
+        <div className="row">
+            <div className="col-12">
+                <h2>Precio del Bitcoin</h2>
+                {console.log(props)}
+            </div>
+
+            <div className="col-md-8">
+                <h2>Noticias sobre el Bitcoin</h2>
+            </div>
+
+            <div className="col-md-4">
+                <h2>Proximos eventos del Bitcoin</h2>
+            </div>
         </div>
     </MasterPage>
 )
+
+Index.getInitialProps = async () => {
+    const precio = await axios.get('https://api.coinmarketcap.com/v2/ticker/1/')
+    return precio.data
+};
 
 export default Index;
